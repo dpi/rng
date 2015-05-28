2d7ead48037fd2c92b0168c0e960ca8ff92f993e
dpi <pro@danielph.in>
2015-05-28 23:40:51 +0800

    Automatically create event node type
    Add option to create a new content type or use existing bundle.
    Added libraries file.
    Added admin css file.
    Fixed #17

--- src/Form/EventTypeConfigForm.php
+++ src/Form/EventTypeConfigForm.php
@@ -11,6 +11,7 @@ use Drupal\Core\Entity\EntityForm;
 use Drupal\Core\Form\FormStateInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Core\Entity\EntityManagerInterface;
+use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\rng\EventManagerInterface;
 
 /**
@@ -26,6 +27,13 @@ class EventTypeConfigForm extends EntityForm {
   protected $entityManager;
 
   /**
+   * The module handler service.
+   *
+   * @var \Drupal\Core\Extension\ModuleHandlerInterface
+   */
+  protected $moduleHandler;
+
+  /**
    * The RNG event manager.
    *
    * @var \Drupal\rng\EventManagerInterface
@@ -37,11 +45,14 @@ class EventTypeConfigForm extends EntityForm {
    *
    * @param \Drupal\Core\Entity\EntityManagerInterface $entity_manager
    *   The entity manager.
+   * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
+   *   The module handler service.
    * @param \Drupal\rng\EventManagerInterface $event_manager
    *   The RNG event manager.
    */
-  public function __construct(EntityManagerInterface $entity_manager, EventManagerInterface $event_manager) {
+  public function __construct(EntityManagerInterface $entity_manager, ModuleHandlerInterface $module_handler, EventManagerInterface $event_manager) {
     $this->entityManager = $entity_manager;
+    $this->moduleHandler = $module_handler;
     $this->eventManager = $event_manager;
   }
 
@@ -51,6 +62,7 @@ class EventTypeConfigForm extends EntityForm {
   public static function create(ContainerInterface $container) {
     return new static(
       $container->get('entity.manager'),
+      $container->get('module_handler'),
       $container->get('rng.event_manager')
     );
   }
@@ -81,7 +93,41 @@ class EventTypeConfigForm extends EntityForm {
         }
       }
 
-      $form['bundle'] = array(
+      if ($this->moduleHandler->moduleExists('node')) {
+        $form['#attached']['library'][] = 'rng/rng.admin';
+        $form['entity_type'] = [
+          '#type' => 'radios',
+          '#options' => NULL,
+          '#title' => $this->t('Event entity type'),
+          '#required' => TRUE,
+        ];
+        $form['entity_type']['node']['radio'] = [
+          '#type' => 'radio',
+          '#title' => $this->t('Create a new content type'),
+          '#description' => $this->t('Create a content type to use as an event type.'),
+          '#return_value' => "node",
+          '#parents' => array('entity_type'),
+          '#default_value' => 'node',
+        ];
+
+        $form['entity_type']['existing']['radio'] = [
+          '#type' => 'radio',
+          '#title' => $this->t('Use existing bundle'),
+          '#description' => $this->t('Use an existing entity/bundle combination.'),
+          '#return_value' => "existing",
+          '#parents' => array('entity_type'),
+          '#default_value' => '',
+        ];
+
+        $form['entity_type']['existing']['container'] = [
+          '#type' => 'container',
+          '#attributes' => [
+            'class' => ['rng-radio-indent'],
+          ],
+        ];
+      }
+
+      $form['entity_type']['existing']['container']['bundle'] = array(
         '#type' => 'select',
         '#title' => $this->t('Bundle'),
         '#options' => $bundle_options,
@@ -110,17 +156,35 @@ class EventTypeConfigForm extends EntityForm {
     return $form;
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $form_state->cleanValues();
     $this->entity = $this->buildEntity($form, $form_state);
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function save(array $form, FormStateInterface $form_state) {
     $event_type_config = $this->getEntity();
 
     if ($event_type_config->isNew()) {
-      $bundle = $form_state->getValue('bundle');
-      list($event_type_config->entity_type, $event_type_config->bundle) = explode('.', $bundle);
+      if ($this->moduleHandler->moduleExists('node') && ($form_state->getValue('entity_type') == 'node')) {
+        $node_type = $this->createContentType('event');
+        $t_args = [
+          '%name' => $node_type->label(),
+          '!link' => $node_type->link(),
+        ];
+        drupal_set_message(t('The content type !link has been added.', $t_args));
+        $event_type_config->entity_type = $node_type->getEntityType()->getBundleOf();
+        $event_type_config->bundle = $node_type->id();
+      }
+      else {
+        $bundle = $form_state->getValue('bundle');
+        list($event_type_config->entity_type, $event_type_config->bundle) = explode('.', $bundle);
+      }
     }
 
     // Set to the access operation for event.
@@ -128,14 +192,43 @@ class EventTypeConfigForm extends EntityForm {
 
     $status = $event_type_config->save();
 
-    $message = ($status == SAVED_UPDATED) ? '%label event type was updated.' : '%label event type was added.';
+    $message = ($status == SAVED_UPDATED) ? '%label event type updated.' : '%label event type added.';
     $url = $event_type_config->urlInfo();
-    $t_args = ['%label' => $event_type_config->label(), 'link' => $this->l(t('Edit'), $url)];
+    $t_args = ['%label' => $event_type_config->id(), 'link' => $this->l(t('Edit'), $url)];
 
     drupal_set_message($this->t($message, $t_args));
     $this->logger('rng')->notice($message, $t_args);
 
     $form_state->setRedirect('rng.event_type_config.overview');
+  }
+
+  /**
+   * Creates a content type.
+   *
+   * Attempts to create a content type with ID $prefix, $prefix_1, $prefix_2...
+   *
+   * @param string $prefix
+   *   A string prefix for the node type ID.
+   *
+   * @return \Drupal\node\NodeTypeInterface
+   *   A node type entity.
+   */
+  private function createContentType($prefix) {
+    // Generate a unique id
+    $i = 0;
+    $separator = '_';
+    $id = $prefix;
+    while ($this->entityManager->getStorage('node_type')->load($id)) {
+      $i++;
+      $id = $prefix . $separator . $i;
+    }
+
+    $node_type = $this->entityManager->getStorage('node_type')->create([
+      'type' => $id,
+      'name' => $id,
+    ]);
+    $node_type->save();
+    return $node_type;
   }
 
 }
