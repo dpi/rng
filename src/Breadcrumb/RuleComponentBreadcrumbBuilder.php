537063183d9caba3ecc4ac0439b6306e3cd54eec
dpi <pro@danielph.in>
2015-06-18 01:56:38 +0800

    Added rule component (action/condition) breadcrumb.

---
+++ src/Breadcrumb/RuleComponentBreadcrumbBuilder.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\rng\Breadcrumb\RuleComponentBreadcrumbBuilder.
+ */
+
+namespace Drupal\rng\Breadcrumb;
+
+use Drupal\Core\Breadcrumb\BreadcrumbBuilderInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\rng\RuleComponentInterface;
+use Drupal\Core\Link;
+
+/**
+ * Provides a breadcrumb builder for RNG rule components.
+ */
+class RuleComponentBreadcrumbBuilder implements BreadcrumbBuilderInterface {
+
+  use StringTranslationTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function applies(RouteMatchInterface $route_match) {
+    $component = $route_match->getParameter('rng_rule_component');
+    return $component instanceof RuleComponentInterface;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(RouteMatchInterface $route_match) {
+    $links = array(Link::createFromRoute($this->t('Home'), '<front>'));
+    /** @var \Drupal\rng\RuleComponentInterface $component */
+    $component = $route_match->getParameter('rng_rule_component');
+    if ($rule = $component->getRule()) {
+      if ($event = $rule->getEvent()) {
+        $links[] = new Link($event->label(), $event->urlInfo());
+      }
+    }
+    return $links;
+  }
+
+}
