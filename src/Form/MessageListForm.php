80309e18c7e9daca05b02004d19bfcf51a9318aa
dpi <pro@danielph.in>
2015-07-25 02:40:10 +0800

    Fixed a bug deleting all non-communication rules when the message list form is saved.

--- src/Form/MessageListForm.php
+++ src/Form/MessageListForm.php
@@ -14,6 +14,7 @@ use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\rng\Plugin\Condition\CurrentTime;
+use Drupal\Core\Entity\EntityInterface;
 
 /**
  * Creates message list form.
@@ -65,6 +66,36 @@ class MessageListForm extends FormBase {
   }
 
   /**
+   * Get a list of rules
+   *
+   * @param \Drupal\Core\Entity\EntityInterface $event
+   *   An event entity.
+   *
+   * @return \Drupal\rng\RuleInterface[]
+   *   An array of rng_rule entities keyed by rule ID.
+   */
+  protected function getCommunicationRules(EntityInterface $event) {
+    // list of communication related action plugin ids.
+    $communication_actions = ['rng_courier_message'];
+    $rules = [];
+
+    $rules_all = $this->eventManager
+      ->getMeta($event)
+      ->getRules(NULL, FALSE, NULL);
+    foreach ($rules_all as $rid => $rule) {
+      foreach ($rule->getActions() as $action) {
+        $action_id = $action->getPluginId();
+        if (in_array($action_id, $communication_actions)) {
+          $rules[$rid] = $rule;
+          continue 2;
+        }
+      }
+    }
+
+    return $rules;
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state, RouteMatchInterface $route_match = NULL, $event = NULL) {
@@ -92,72 +123,60 @@ class MessageListForm extends FormBase {
 
     $destination = $this->redirectDestination->getAsArray();
     $form['#rng_event'] = $route_match->getParameter($event);
-    $rules = $this->eventManager
-      ->getMeta($form['#rng_event'])
-      ->getRules(NULL, FALSE, NULL);
-
-    // list of communication related action plugin ids.
-    $communication_actions = ['rng_courier_message'];
-    foreach ($rules as $rid => $rule) {
+    foreach ($this->getCommunicationRules($form['#rng_event']) as $rid => $rule) {
       foreach ($rule->getActions() as $action) {
         $row = [];
         $links = [];
-        $action_id = $action->getPluginId();
-        if (in_array($action_id, $communication_actions)) {
-          // @todo: move trigger definitions to a discovery service.
-          $trigger_id = $rule->getTriggerID();
-          $row['trigger']['#markup'] = isset($rng_triggers[$trigger_id]) ? $rng_triggers[$trigger_id] : $trigger_id;
+        // @todo: move trigger definitions to a discovery service.
+        $trigger_id = $rule->getTriggerID();
+        $row['trigger']['#markup'] = isset($rng_triggers[$trigger_id]) ? $rng_triggers[$trigger_id] : $trigger_id;
 
-          $row['date']['#markup'] = $this->t('N/A');
-          foreach ($rule->getConditions() as $component) {
-            $condition = $component->createInstance();
-            if ($condition instanceof CurrentTime) {
-              $row['date']['#markup'] = $condition->getDateFormatted();
-              if ($component->access('edit')) {
-                $links['edit-date'] = [
-                  'title' => $this->t('Edit date'),
-                  'url' => $component->urlInfo('edit-form'),
-                  'query' => $destination,
-                ];
-              }
+        $row['date']['#markup'] = $this->t('N/A');
+        foreach ($rule->getConditions() as $component) {
+          $condition = $component->createInstance();
+          if ($condition instanceof CurrentTime) {
+            $row['date']['#markup'] = $condition->getDateFormatted();
+            if ($component->access('edit')) {
+              $links['edit-date'] = [
+                'title' => $this->t('Edit date'),
+                'url' => $component->urlInfo('edit-form'),
+                'query' => $destination,
+              ];
             }
           }
+        }
 
-          $row['status'] = [
-            '#type' => 'checkbox',
-            '#title' => $this->t('Active'),
-            '#title_display' => 'invisible',
-            '#default_value' => (bool) $rule->isActive(),
-            '#wrapper_attributes' => [
-              'class' => [
-                'checkbox',
-              ],
+        $row['status'] = [
+          '#type' => 'checkbox',
+          '#title' => $this->t('Active'),
+          '#title_display' => 'invisible',
+          '#default_value' => (bool) $rule->isActive(),
+          '#wrapper_attributes' => [
+            'class' => [
+              'checkbox',
             ],
-          ];
+          ],
+        ];
 
-          if ($action->access('edit')) {
-            $links['edit-templates'] = [
-              'title' => $this->t('Edit templates'),
-              'url' => $action->urlInfo('edit-form'),
-              'query' => $destination,
-            ];
-          }
-          if ($rule->access('delete')) {
-            $links['delete'] = [
-              'title' => $this->t('Delete'),
-              'url' => $rule->urlInfo('delete-form'),
-              'query' => $destination,
-            ];
-          }
-
-          $row['operations']['data'] = [
-            '#type' => 'operations',
-            '#links' => $links,
+        if ($action->access('edit')) {
+          $links['edit-templates'] = [
+            'title' => $this->t('Edit templates'),
+            'url' => $action->urlInfo('edit-form'),
+            'query' => $destination,
           ];
         }
-        else {
-          continue;
+        if ($rule->access('delete')) {
+          $links['delete'] = [
+            'title' => $this->t('Delete'),
+            'url' => $rule->urlInfo('delete-form'),
+            'query' => $destination,
+          ];
         }
+
+        $row['operations']['data'] = [
+          '#type' => 'operations',
+          '#links' => $links,
+        ];
 
         $form['action_list'][$rid] = $row;
       }
@@ -177,11 +196,7 @@ class MessageListForm extends FormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $form_rules = $form_state->getValue('action_list');
-    $rules = $this->eventManager
-      ->getMeta($form['#rng_event'])
-      ->getRules(NULL, FALSE, NULL);
-
-    foreach ($rules as $rid => $rule) {
+    foreach ($this->getCommunicationRules($form['#rng_event']) as $rid => $rule) {
       $enabled = !empty($form_rules[$rid]['status']);
       if ($rule->isActive() != $enabled) {
         $rule
