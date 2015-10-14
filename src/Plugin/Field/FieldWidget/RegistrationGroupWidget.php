8d465a6128b947863ff584e53524e316d2511a24
dpi <pro@danielph.in>
2015-10-14 15:35:43 +0800

    Added stub field widgets for registration_type and registration_group entity reference fields.
    #62
    #52

---
+++ src/Plugin/Field/FieldWidget/RegistrationGroupWidget.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\rng\Plugin\Field\FieldWidget\RegistrationGroupWidget.
+ */
+
+namespace Drupal\rng\Plugin\Field\FieldWidget;
+
+use Drupal\Core\Field\Plugin\Field\FieldWidget\OptionsButtonsWidget;
+
+/**
+ * Widget for `registration group` entity reference fields.
+ *
+ * @FieldWidget(
+ *   id = "rng_registration_group",
+ *   label = @Translation("Registration group checkboxes"),
+ *   field_types = {
+ *     "entity_reference",
+ *   },
+ *   multiple_values = TRUE
+ * )
+ */
+class RegistrationGroupWidget extends OptionsButtonsWidget {
+  // @todo Stub, to be expanded later. See: https://github.com/dpi/rng/issues/62
+}
---
+++ src/Plugin/Field/FieldWidget/RegistrationTypeWidget.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\rng\Plugin\Field\FieldWidget\RegistrationTypeWidget.
+ */
+
+namespace Drupal\rng\Plugin\Field\FieldWidget;
+
+use Drupal\Core\Field\Plugin\Field\FieldWidget\OptionsButtonsWidget;
+
+/**
+ * Widget for `registration type` entity reference fields.
+ *
+ * @FieldWidget(
+ *   id = "rng_registration_type",
+ *   label = @Translation("Registration type checkboxes"),
+ *   field_types = {
+ *     "entity_reference",
+ *   },
+ *   multiple_values = TRUE
+ * )
+ */
+class RegistrationTypeWidget extends OptionsButtonsWidget {
+  // @todo Stub, to be expanded later. See: https://github.com/dpi/rng/issues/62
+}
