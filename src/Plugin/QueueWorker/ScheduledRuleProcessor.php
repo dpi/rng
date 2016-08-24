c9255d391b391a7fd84f9d5b2db555802be2249d
dpi <pro@danielph.in>
2016-08-25 00:06:49 +0800

    Injected event manager service into ScheduledRuleProcessor. Added extra guards in ScheduledRuleProcessor in case the rule schedule entity was deleted.

--- src/Plugin/QueueWorker/ScheduledRuleProcessor.php
+++ src/Plugin/QueueWorker/ScheduledRuleProcessor.php
@@ -7,7 +7,10 @@
 
 namespace Drupal\rng\Plugin\QueueWorker;
 
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Core\Queue\QueueWorkerBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\rng\EventManagerInterface;
 use Drupal\rng\Entity\RuleSchedule;
 
 /**
@@ -19,7 +22,43 @@ use Drupal\rng\Entity\RuleSchedule;
  *   cron = {"time" = 60}
  * )
  */
-class ScheduledRuleProcessor extends QueueWorkerBase {
+class ScheduledRuleProcessor extends QueueWorkerBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The RNG event manager.
+   *
+   * @var \Drupal\rng\EventManagerInterface
+   */
+  protected $eventManager;
+
+  /**
+   * Constructs a ScheduledRuleProcessor object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\rng\EventManagerInterface $event_manager
+   *   The RNG event manager.
+   */
+  public function __construct(array $configuration, $plugin_id, array $plugin_definition, EventManagerInterface $event_manager) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->eventManager = $event_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('rng.event_manager')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -32,16 +71,16 @@ class ScheduledRuleProcessor extends QueueWorkerBase {
       return;
     }
 
-    /** @var \Drupal\rng\EventManagerInterface $event_manager */
-    $event_manager = \Drupal::service('rng.event_manager');
+    if (!$rule_schedule = RuleSchedule::load($data['rule_scheduler_id'])) {
+      return;
+    }
 
-    $rule_schedule = RuleSchedule::load($data['rule_scheduler_id']);
     $rule_schedule->incrementAttempts();
     $rule_schedule->save();
 
     if (($component = $rule_schedule->getComponent()) && ($rule = $component->getRule())) {
       $event = $rule->getEvent();
-      $event_meta = $event_manager->getMeta($event);
+      $event_meta = $this->eventManager->getMeta($event);
       $context = [
         'event' => $event,
         'registrations' => $event_meta->getRegistrations()
